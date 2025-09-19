295fab99035bcd73a419aafb1deaf047532073ae
Tarunsaxena177 <tarunsaxena111162@gmail.com>
2025-09-19 11:01:06 +0530

    task omcplete

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://bitsandvolts-tarun-1.onrender.com"
+      "/api": "http://localhost:8000"
     }
   }
 });
