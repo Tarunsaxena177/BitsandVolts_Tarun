9ae5db35596ea7e0a88e5df68d3f880ee7421320
Tarunsaxena177 <tarunsaxena111162@gmail.com>
2025-09-19 10:59:20 +0530

    Task Completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:8000"
+      "/api": "https://bitsandvolts-tarun-1.onrender.com"
     }
   }
 });
